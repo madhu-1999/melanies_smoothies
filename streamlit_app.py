b2bf3000337b89aa2348a365d9356ef682ee8f68
madhu-1999 <67584344+madhu-1999@users.noreply.github.com>
2026-05-22 18:00:54 -0700

    Fix smoothie order success message indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
 
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
@@ -34,7 +39,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
-
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    
