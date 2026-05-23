c5f28ba98074aa2513d9b9d31e2f19cf40875adb
madhu-1999 <67584344+madhu-1999@users.noreply.github.com>
2026-05-22 18:25:42 -0700

    Fix syntax error in fruit options selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ name_on_order = st.text_input('Name on Smoothie')
 st.write("The name on your smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table('smoothies.public.fruit_options').select(col('fruit_name'), col('search_on))
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('fruit_name'), col('search_on'))
 pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
